a0da18d849d8b954266bd7911e706206944a9b74
Dennis Chen <dennis@dennischen.com>
2025-02-10 11:58:52 -0800

    Remove non api related files

--- config/routes.rb
+++ config/routes.rb
@@ -9,12 +9,7 @@ Rails.application.routes.draw do
     post "process", to: "receipts#process_receipt"
     get ":id/points", to: "receipts#points"
   end
-
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
+  
   # Defines the root path route ("/")
   # root "posts#index"
 end
